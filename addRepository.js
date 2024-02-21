05def54504a479e55e730df2a3252028aa01f362
Vovensky <102856478+Vovensky@users.noreply.github.com>
2024-02-21 17:35:10 +0500

    rework oktokit to fetch

--- addRepository.js
+++ addRepository.js
@@ -11,10 +11,11 @@ function addRepository(object, node, delCallBack) {
 
     for(let i=0; i<3; i++) {
       let value;
-      value = keys[i] == 'owner' ? object[keys[i]].login : object[keys[i]]
+      value = keys[i] == 'owner' ? object[keys[i]].login : object[keys[i]];
+      let title = keys[i] == 'stargazers_count' ? 'stars' : keys[i];
       let divText = document.createElement('div');
       divText.classList.add('repositoriesText');
-      divText.textContent = `${keys[i]}: ${value}`;
+      divText.textContent = `${title}: ${value}`;
       divWrapper.appendChild(divText)
     }
 
